0cefa6df46b78146e2ee326b120ca77a7cf26bd1
yszhao2004 <130468895+yszhao2004@users.noreply.github.com>
2024-08-30 15:41:09 +0800

    Add files via upload

---
+++ pred_ls.py
@@ -0,0 +1,182 @@
+import os
+from datasets import load_dataset
+import torch
+import json
+from transformers import AutoTokenizer, LlamaTokenizer, LlamaForCausalLM, AutoModelForCausalLM
+from tqdm import tqdm
+import numpy as np
+import random
+import argparse
+
+from longstreaming.stm_utils import load, download_url, load_jsonl
+from longstreaming.stm_enable_streaming_llm import enable_streaming_llm
+
+from functools import partial
+
+@torch.no_grad()
+def greedy_generate(model, tokenizer, input_ids, past_key_values, max_gen_len):
+    outputs = model(
+        input_ids=input_ids,
+        past_key_values=past_key_values,
+        use_cache=True,
+    )
+    past_key_values = outputs.past_key_values
+    pred_token_idx = outputs.logits[:, -1, :].argmax(dim=-1).unsqueeze(1)
+    generated_ids = [pred_token_idx.item()]
+    generated_text = []
+    pos = 0
+    for _ in range(max_gen_len - 1):
+        outputs = model(
+            input_ids=pred_token_idx,
+            past_key_values=past_key_values,
+            use_cache=True,
+        )
+        past_key_values = outputs.past_key_values
+        pred_token_idx = outputs.logits[:, -1, :].argmax(dim=-1).unsqueeze(1)
+        generated_ids.append(pred_token_idx.item())
+
+        decoded_text = tokenizer.decode(
+            generated_ids[-1:],
+            skip_special_tokens=True,
+            clean_up_tokenization_spaces=True,
+            spaces_between_special_tokens=False,
+        ).strip()
+        generated_text.append(decoded_text)
+
+        now = len(generated_text) - 1
+        if now > pos:
+            pos = now
+
+        if pred_token_idx == tokenizer.eos_token_id:
+            break
+
+
+    final_generated_text = " ".join(generated_text).strip()
+    return past_key_values, final_generated_text
+
+@torch.no_grad()
+def streaming_inference(model, tokenizer, prompts, kv_cache=None, max_gen_len=1000):
+    all_generated_texts = []
+    past_key_values = None
+
+    for idx, prompt in enumerate(prompts):
+        
+        input_ids = tokenizer(prompt, return_tensors="pt").input_ids
+        input_ids = input_ids.to(model.device)
+        seq_len = input_ids.shape[1]
+
+        if kv_cache is not None:
+            space_needed = seq_len + max_gen_len
+            past_key_values = kv_cache.evict_for_space(past_key_values, space_needed)
+
+        past_key_values, generated_text = greedy_generate(
+            model, tokenizer, input_ids, past_key_values, max_gen_len=max_gen_len
+        )
+
+        all_generated_texts.append(generated_text)
+
+    return all_generated_texts
+
+def parse_args(args=None):
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--model', type=str, default="vicuna-v1.5-7b-ls")
+    parser.add_argument('--e', action='store_true', help="Evaluate on LongBench-E")
+    return parser.parse_args(args)
+
+# This is the customized building prompt for chat models
+def build_chat(tokenizer, prompt, model_name):
+    from fastchat.model import get_conversation_template
+    conv = get_conversation_template("vicuna")
+    conv.append_message(conv.roles[0], prompt)
+    conv.append_message(conv.roles[1], None)
+    prompt = conv.get_prompt()
+    return prompt
+
+
+def get_pred(data, max_length, max_gen, prompt_format, dataset, device, model_name, model2path, out_path):
+    model, tokenizer = load_model_and_tokenizer(model2path[model_name], model_name, device)
+    kv_cache = None
+    for json_obj in tqdm(data):
+        prompt = prompt_format.format(**json_obj)
+        # truncate to fit max_length (we suggest truncate in the middle, since the left and right side may contain crucial instructions)
+        tokenized_prompt = tokenizer(prompt, truncation=False, return_tensors="pt").input_ids[0]
+        if len(tokenized_prompt) > max_length:
+            half = int(max_length/2)
+            prompt = tokenizer.decode(tokenized_prompt[:half], skip_special_tokens=True)+tokenizer.decode(tokenized_prompt[-half:], skip_special_tokens=True)
+        if dataset not in ["trec", "triviaqa", "samsum", "lsht", "lcc", "repobench-p"]: # chat models are better off without build prompts on these tasks
+            prompt = build_chat(tokenizer, prompt, model_name)
+
+        prompts = [prompt] 
+        kv_cache = enable_streaming_llm(model, start_size=4, recent_size=2000)
+
+        generated_texts = streaming_inference(model, tokenizer, prompts, kv_cache=kv_cache, max_gen_len=max_gen)
+
+        pred = generated_texts[0] 
+        with open(out_path, "a", encoding="utf-8") as f:
+            json.dump({"pred": pred, "answers": json_obj["answers"], "all_classes": json_obj["all_classes"], "length": json_obj["length"]}, f, ensure_ascii=False)
+            f.write('\n')
+
+def seed_everything(seed):
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    np.random.seed(seed)
+    random.seed(seed)
+    torch.backends.cudnn.benchmark = False
+    torch.backends.cudnn.deterministic = True
+    torch.cuda.manual_seed_all(seed)
+
+def load_model_and_tokenizer(path, model_name, device):
+    model = AutoModelForCausalLM.from_pretrained(path,torch_dtype=torch.bfloat16).to(device)
+    tokenizer = AutoTokenizer.from_pretrained(path, trust_remote_code=True, use_fast=False)
+    model = model.eval()
+    return model, tokenizer
+
+def load_local_dataset(path):
+    data = []
+    with open(path, 'r') as f:
+        for line in f:
+            data.append(json.loads(line))
+    return data
+
+if __name__ == '__main__':
+    seed_everything(42)
+    args = parse_args()
+
+    model2path = json.load(open("config/model2path.json", "r"))
+    model2maxlen = json.load(open("config/model2maxlen.json", "r"))
+    device = torch.device('cuda:0')
+    model_name = args.model
+    max_length = model2maxlen[model_name]
+
+    if args.e:
+        datasets = ["qasper", "multifieldqa_en", "hotpotqa", "2wikimqa", "gov_report", "multi_news", \
+            "trec", "triviaqa", "samsum", "passage_count", "passage_retrieval_en", "lcc", "repobench-p"]
+    else:
+        datasets = ["narrativeqa", "qasper", "multifieldqa_en", "multifieldqa_zh", "hotpotqa", "2wikimqa", "musique", \
+                    "dureader", "gov_report", "qmsum", "multi_news", "vcsum", "trec", "triviaqa", "samsum", "lsht", \
+                    "passage_count", "passage_retrieval_en", "passage_retrieval_zh", "lcc", "repobench-p"]
+
+    # we design specific prompt format and max generation length for each task, feel free to modify them to optimize model output
+    dataset2prompt = json.load(open("config/dataset2prompt.json", "r"))
+    dataset2maxlen = json.load(open("config/dataset2maxlen.json", "r"))
+    # predict on each dataset
+
+    if not os.path.exists("pred"):
+        os.makedirs("pred")
+    if not os.path.exists("pred_e"):
+        os.makedirs("pred_e")
+    for dataset in datasets:
+        if args.e:
+            data = load_local_dataset(f'./data/data/{dataset}_e.jsonl')
+            if not os.path.exists(f"pred_e/{model_name}"):
+                os.makedirs(f"pred_e/{model_name}")
+            out_path = f"pred_e/{model_name}/{dataset}.jsonl"
+        else:
+            data = load_local_dataset(f'./data/data/{dataset}.jsonl')
+            if not os.path.exists(f"pred/{model_name}"):
+                os.makedirs(f"pred/{model_name}")
+            out_path = f"pred/{model_name}/{dataset}.jsonl"
+        prompt_format = dataset2prompt[dataset]
+        max_gen = dataset2maxlen[dataset]
+
+        get_pred(data, max_length, max_gen, prompt_format, dataset, device, model_name, model2path, out_path)
