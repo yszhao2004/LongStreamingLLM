8a2fab80c850b3765a00f558c878de57b1e0920b
yszhao2004 <130468895+yszhao2004@users.noreply.github.com>
2024-08-30 15:36:58 +0800

    upload combine

---
+++ combine/stm_enable_streaming_llm.py
@@ -0,0 +1,18 @@
+from stm_kv_cache import StartRecentKVCache
+import long_llama_self_extend_patch as LlamaSE
+from long_modify_utils import modify_method_of_instance
+from functools import partial
+
+self_extend_forward = partial(LlamaSE.self_extend_forward, group_size_1=8, group_size_2=1024)
+
+def enable_streaming_llm(model, start_size, recent_size):
+    k_seq_dim = v_seq_dim = 2
+    modify_method_of_instance(model, "LlamaAttention", "forward", self_extend_forward)
+
+    kv_cache = StartRecentKVCache(
+        start_size=start_size,
+        recent_size=recent_size,
+        k_seq_dim=k_seq_dim,
+        v_seq_dim=v_seq_dim,
+    )
+    return kv_cache
